d1232898e08216b77151f520be45e247018bae94
Zoheth <z0heth@outlook.com>
2023-10-02 19:34:09 +0800

    Use premake to create projects

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Siho"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Siho"
+	location "Siho"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"SH_PLATFORM_WINDOWS",
+			"SH_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "SH_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SH_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "SH_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"Siho/vendor/spdlog/include",
+		"Siho/src"
+	}
+
+	links
+	{
+		"Siho"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"SH_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "SH_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SH_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "SH_DIST"
+		optimize "On"
