d42954c98f1aed729dc623eb503b0a5c73ee5b85
Zoheth <z0heth@outlook.com>
2023-11-05 22:01:39 +0800

    Cleaning up...

--- Siho/src/Siho/imgui/imgui_build.cpp
+++
@@ -1,5 +0,0 @@
-#include "shpch.h"
-
-#define IMGUI_IMPL_OPENGL_LOADER_CUSTOM
-#include "backends/imgui_impl_vulkan.cpp"
-#include "backends/imgui_impl_glfw.cpp"
--- Siho/src/Siho/utilities/StringUtils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace Siho::Utils {
-	std::string ReadFileAndSkipBom(const std::filesystem::path& filepath);
-
-	// Keeps delimiters except for spaces, used for shaders
-	std::vector<std::string> SplitStringAndKeepDelims(std::string str);
-}
